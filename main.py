53713856d4f72cdd5ff8a97217fa3979d05050d1
mishka251 <mishkabelka251@gmail.com>
2019-11-28 03:14:31 +0500

    Добавлены методы

--- main.py
+++ main.py
@@ -5,26 +5,53 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from fcmeans import FCM
+from sklearn.metrics import silhouette_samples, silhouette_score, davies_bouldin_score
 from seaborn import scatterplot as scatter
+from yellowbrick.cluster import KElbowVisualizer
+
+from sklearn.cluster import DBSCAN
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+from pyclustering.cluster import cluster_visualizer
+from pyclustering.cluster.birch import birch
+from pyclustering.samples.definitions import FCPS_SAMPLES
+from pyclustering.utils import read_sample
 
 
-#---------------- Метод локтя ------------------------------------------------------------------
+# ---------------- Метод локтя ------------------------------------------------------------------
 def ElbowMethod(X):
-    distortions = []
-    for i in range(1, 11):
-        km = KMeans(n_clusters=i, #задаем число кластеров
-                    init='k-means++', # Метод инициализации, {'k-means++', 'random' или ndarray}
-                    n_init=10, #Количество раз, когда алгоритм k-means будет выполняться с разными значениями центров кластеров. Окончательные результаты будут самым лучшим выходом n_init последовательно работает в условиях инерции.
-                    max_iter=300, # Максимальное число итераций алгоритма k-средних для a одиночная пробежка.
-                    random_state=0) # Determines random number generation for centroid initialization. Use an int to make the randomness deterministic.
-        km.fit(X)
-        distortions.append(km.inertia_)
-    print('Искажение : %.2f ' % km.inertia_)
-    plt.plot(range(1, 11), distortions, marker='o')
-    plt.xlabel('Чиcлo кластеров')
-    plt.ylabel('Искажение')
+    model = KMeans()
+    visualizer1 = KElbowVisualizer(model, k=(1, 15), timings=False)
+
+    visualizer1.fit(X)  # Fit the data to the visualizer
+    visualizer1.show()  # Finalize and render the figure
+
+
+def silhouette_method(X):
+    poss_n = range(2, 10)
+    results = []
+    for n in poss_n:
+        clusterer = KMeans(n_clusters=n)
+        cluster_labels = clusterer.fit_predict(X)
+        silhouette_avg = silhouette_score(X, cluster_labels, metric='euclidean')
+        results.append(silhouette_avg)
+    plt.plot(poss_n, results)
     plt.show()
-    print("ElbowMethod")
+
+
+def dbi(X):
+    poss_n = range(2, 10)
+    results = []
+    for n in poss_n:
+        clusterer = KMeans(n_clusters=n)
+        cluster_labels = clusterer.fit_predict(X)
+        silhouette_avg = davies_bouldin_score(X, cluster_labels)
+        results.append(silhouette_avg)
+    plt.plot(poss_n, results)
+    plt.xlabel('K')
+    plt.show()
+
 
 def load_data(file_name):
     birds = pd.read_csv(file_name).dropna(axis=1, how='all')
@@ -42,12 +69,34 @@ def clustering(data, n_clusters):
     print(any(map(lambda row: any(map(lambda el: el is None or el is float("-inf") or el is float("inf"), row)), X)))
     km = KMeans(n_clusters=n_clusters)
 
-    print(X)
-    print(np.any(np.isnan(X)))
-    print(np.any(np.isinf(X)))
+    # print(X)
 
     clusters = km.fit_predict(data)
-    data['cluster'] = clusters
+    print("k-means")
+    print(clusters)
+    return clusters
+    # data['cluster'] = clusters
+
+
+def clustering2(data, n_clusters):
+    data2 = data.to_numpy().tolist()
+    # print(data2)
+    # Create BIRCH algorithm to allocate three clusters.
+    birch_instance = birch(data2, n_clusters)
+    # Run cluster analysis.
+    birch_instance.process()
+    # Get allocated clusters.
+    print("birch")
+    objects_in_clusters = birch_instance.get_clusters()
+    print(objects_in_clusters)
+    clusters = []
+    for i in range(len(data2)):
+        indexes = [i2 for i2, v in enumerate(objects_in_clusters) if v.count(i) > 0]
+        # print(indexes)
+        clusters.append(indexes[0])
+
+    print(clusters)
+    return clusters
 
 
 def add_indexes(data, values):
@@ -59,35 +108,55 @@ def add_indexes(data, values):
     data['res_indexes'] = list(map(lambda type: indexes[type], data['test_result']))
 
 
+def str_to_indexes(values):
+    indexes = {}
+    for name in values:
+        if name not in indexes:
+            indexes[name] = len(indexes)
+
+    return list(map(lambda v: indexes[v], values))
+
+
 def fuzzy(data, n_clusters):
     fcm = FCM(n_clusters)
     fcm.fit(data)
-
+    print("fuzzy")
     print(fcm.centers)
     print(fcm.u)
+    return fcm.u
     # plot result
-    #% matplotlib
-    #inline
-    #f, axes = plt.subplots(1, 2, figsize=(11, 5))
-    #scatter(data[:, 0], data1[:, 1], ax=axes[0])
-    #scatter(X[:, 0], X[:, 1], ax=axes[1], hue=fcm_labels)
-    #scatter(fcm_centers[:, 0], fcm_centers[:, 1], ax=axes[1], marker="s", s=200)
-    #plt.show()
+    # % matplotlib
+    # inline
+    # f, axes = plt.subplots(1, 2, figsize=(11, 5))
+    # scatter(data[:, 0], data1[:, 1], ax=axes[0])
+    # scatter(X[:, 0], X[:, 1], ax=axes[1], hue=fcm_labels)
+    # scatter(fcm_centers[:, 0], fcm_centers[:, 1], ax=axes[1], marker="s", s=200)
+    # plt.show()
+
+
+def calc_clusters_cnt(data):
+    ElbowMethod(data)
+    silhouette_method(data)
+    dbi(data)
+
 
 def main():
     file_name = "bird.csv"
     birds, data = load_data(file_name)
-    ElbowMethod(data)
-    clustering(data, n_clusters=5)
-    fuzzy(data, n_clusters=5)
-    data['test_result'] = birds['type']
-    add_indexes(data, birds['type'])
+    calc_clusters_cnt(data)
 
-    print(data)
+    clusters1 = clustering(data, n_clusters=5)
+    clusters2 = clustering2(data, n_clusters=5)
+    fuzzy_props = fuzzy(data, n_clusters=5)
+    # data['test_result'] = birds['type']
+    # add_indexes(data, birds['type'])
 
-    print(np.corrcoef(data['cluster'], data['res_indexes']))
+    # print(data)
+    types_indexes = str_to_indexes(birds['type'])
+    print(np.corrcoef(clusters1, clusters2))
+    print(np.corrcoef(types_indexes, clusters2))
+    print(np.corrcoef(clusters1, types_indexes))
 
-    #fuzzy(data, n_clusters=5)
 
 
 main()
